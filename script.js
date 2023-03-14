bc4ad90c1984cafdaec7ff8a73ded6731909906d
sam-starikov <sam_starikov@rambler.ru>
2023-03-14 22:56:43 +0300

    added chart js and complete second section

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const ctx = document.getElementById('myChart')
 new Chart(ctx, {
    type: 'line',
    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Mon', 'Thu', 'Wen', 'Thru', 'Fri', 'Sab', 'Sun'],
       datasets: [
          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'WorkOut',
+            data: [9, 13, 1, 3, 7, 2, 6, 7, 4, 8, 0],
             borderWidth: 1,
+            backgroundColor: ['#e01e2e'],
+            borderColor: ['#e01e2e'],
          },
       ],
    },
