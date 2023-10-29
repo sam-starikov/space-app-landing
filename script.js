4f04dccc63d64dc06aefa9af047546025850c8ce
sam-starikov <sam_starikov@rambler.ru>
2023-10-29 20:16:25 +0400

    refactoring

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
 const ctx = document.getElementById('myChart')
 
 new Chart(ctx, {
-   type: 'line',
-   data: {
-      labels: ['Mon', 'Thu', 'Wen', 'Thru', 'Fri', 'Sab', 'Sun'],
-      datasets: [
-         {
-            label: 'WorkOut',
-            data: [9, 13, 1, 3, 7, 2, 6, 7, 4, 8, 0],
-            borderWidth: 1,
-            backgroundColor: ['#e01e2e'],
-            borderColor: ['#e01e2e'],
-         },
-      ],
-   },
-   options: {
-      scales: {
-         y: {
-            beginAtZero: true,
-         },
-      },
-   },
+	type: 'line',
+	data: {
+		labels: [
+			'2016',
+			'2017',
+			'2018',
+			'2019',
+			'2020',
+			'2021',
+			'2022',
+			'2023',
+			'2024',
+		],
+		datasets: [
+			{
+				label: 'SpaceApp',
+				data: [9, 13, 1, 3, 7, 2, 6, 7, 4, 8, 0],
+				borderWidth: 1,
+				backgroundColor: ['#e01e2e'],
+				borderColor: ['#e01e2e'],
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
 })
-/* ---------------------------------- */
-
-const btn = document.querySelector('.button')
-const itemAnalys = document.querySelector('#item-analys')
-const mainTitle = document.querySelector('#main-title')
-const arrText = ['Анализирую', 'AI-Driven Analytics Platform']
-
-btn.addEventListener('click', () => btn.classList.toggle('test'))
-
-itemAnalys.addEventListener('click', () => {
-   mainTitle.textContent === `AI-Driven Analytics Platform`
-      ? (mainTitle.textContent = `Анализирую`)
-      : (mainTitle.textContent = `AI-Driven Analytics Platform`)
-})
-
-
